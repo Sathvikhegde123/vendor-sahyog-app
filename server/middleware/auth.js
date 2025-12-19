863e0d82cf0821d7941a24159952bfc5f28f3802
Sathvikhegde123 <sathvik.hegde17@gmail.com>
2025-12-19 12:00:31 +0530

    refator auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,16 @@
 import jwt from "jsonwebtoken";
 
 const auth = (req, res, next) => {
-  console.log("AUTH HEADER:", req.headers.authorization);
 
   const token = req.headers.authorization?.split(" ")[1];
 
-  console.log("EXTRACTED TOKEN:", token);
-
+ 
   if (!token) {
     return res.status(401).json({ error: "No token provided" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("DECODED TOKEN:", decoded);
 
     req.user = decoded;
     next();
